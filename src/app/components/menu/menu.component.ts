b2a46e040c990099e2b303b61854f08d77edcb70
Francis Mendoza Chavez <francisgfmusic@gmail.com>
2022-06-06 22:50:13 -0500

    Mejoras navegacion mobile
    - se controla la dimencion de la pantalla con js

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,64 +8,130 @@ import { left } from '@popperjs/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+ 
+  constructor(private router: Router) {
 
-  constructor( private router:Router) { 
-   this.ChangeBackTransparentNav();
+    
+    this.ChangeBackTransparentNav();
+    /*this.ValidateResolution();*/
+    this.CaptionMedia();
   }
 
   ngOnInit(): void {
-  }
-  navContacto(){
-    this.router.navigate(['/contacto'])  
-    this.HideMenu();   
-    this.ChangeBackTransparentNav();  
-  }
-  navHome(){
-    this.router.navigate(['/home'])
-    this.HideMenu();
-    this.ChangeBackTransparentNav(); 
-  }
-  navHabilidades(){
-    this.router.navigate(['/habilidades'])
-    this.HideMenu();
-    this.ChangeBackTransparentNav(); 
-  }
-  navHobies(){
-    this.router.navigate(['/hobies'])
-    this.HideMenu();
-    this.ChangeBackTransparentNav(); 
-  }
-  navExperiencia(){
-    this.router.navigate(['/experiencia'])
-    this.HideMenu();
-    this.ChangeBackTransparentNav(); 
+    
   }
 
-  showMenu(){
 
-   // if(window.innerWidth > 800){
-   //$("nav ul").fadeIn(500);
-   $("nav ul").toggle("slow");
-   //this.ChangeBackSolidNav();
-   $("nav").css("background-color", "#0f2c5a");
-   /*$(".menu i").replaceWith("<i class=></i>");
-   $(".menu i").attr('_ngcontent-xrx-c53');
-   $(".menu i").attr('class','bi bi-arrow-bar-up').before;*/
 
-   // }
-  }
-  HideMenu(){
-   /* $("nav ul").hide();*/
+  navContacto() {
+    
+    var mediaqueryList = window.matchMedia("(min-width: 1280px) and (max-width: 1920px)");
+    if(mediaqueryList.matches) {
+      this.router.navigate(['/contacto'])
+      this.ChangeBackTransparentNav();
+    }else{
+      $("nav ul").hide();
+      this.router.navigate(['/contacto'])
+    }
   }
 
-  ChangeBackTransparentNav(){
+  navHome() {
+    var mediaqueryList = window.matchMedia("(min-width: 1280px) and (max-width: 1920px)");
+    if(mediaqueryList.matches) {
+      this.router.navigate(['/home'])
+      this.ChangeBackTransparentNav();
+    }else{
+      $("nav ul").hide();
+      this.router.navigate(['/home'])
+    }
+  }
+
+  navHabilidades() {
+    var mediaqueryList = window.matchMedia("(min-width: 1280px) and (max-width: 1920px)");
+    if(mediaqueryList.matches) {
+      this.router.navigate(['/habilidades'])
+      this.ChangeBackTransparentNav();
+    }else{
+      $("nav ul").hide();
+      this.router.navigate(['/habilidades'])
+    }
+  }
+
+
+  navHobies() {
+    var mediaqueryList = window.matchMedia("(min-width: 1280px) and (max-width: 1920px)");
+    if(mediaqueryList.matches) {
+      this.router.navigate(['/hobies'])
+    this.ChangeBackTransparentNav();
+    }else{
+      $("nav ul").hide();
+      this.router.navigate(['/hobies'])
+    }
+
+  }
+  navExperiencia() {
+
+    var mediaqueryList = window.matchMedia("(min-width: 1280px) and (max-width: 1920px)");
+    if(mediaqueryList.matches) {
+      this.router.navigate(['/experiencia'])
+      this.ChangeBackTransparentNav();
+    }else{
+      $("nav ul").hide();
+      this.router.navigate(['/experiencia'])
+    }
+  }
+
+  showMenu() {
+    $("nav ul").toggle("slow");
+    this.ChangeBackSolidNav();
+    $("nav").css("background-color", "#0f2c5a");
+  }
+  
+  HideMenu() {
+    $("nav ul").hide();
+  }
+
+  ChangeBackTransparentNav() {
     $("nav").removeClass("changeBackSolidColor")
     $("nav").addClass("changeBackColor")
   }
 
-  ChangeBackSolidNav(){
+  ChangeBackSolidNav() {
     $("nav").removeClass("changeBackColor")
     $("nav").addClass("changeBackSolidColor")
-   
+
   }
+/*
+  ValidateResolution() {
+    if (screen.width < 1024) {
+
+
+      $("nav").prop("enabled");
+      alert("Pequeña")
+    } else if (screen.width < 1280) {
+      $("nav").prop("enabled");
+      alert("Mediana")
+    } else {
+      $("nav").prop("enabled");
+      alert("Grande")
+    }
+  }*/
+
+CaptionMedia(){
+
+  $(window).resize(function(){
+    var mediaqueryList = window.matchMedia("(min-width: 1280px) and (max-width: 1920px)");
+    if(mediaqueryList.matches) {
+     // alert('La media query se cumple');
+      $("nav ul").show();
+    }else{
+      $("nav ul").hide();
+    }
+    })
+
+
+
+  
+}
+  
 }
